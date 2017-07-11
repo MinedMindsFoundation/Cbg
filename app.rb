139af5f68ba3795464a7bebc0f0fdd2545a99955
heathershockney <shockney@atlanticbb.net>
2017-07-11 09:57:20 -0400

    got rid of merge issues, mumbo jumbo code from pages

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'pg'
 require 'pony'
 
 load './local_env.rb' if File.exist?('./local_env.rb')
-<<<<<<< HEAD
+
 
 def db()
   db_params = {
@@ -20,10 +20,9 @@ get '/' do
 
 message = params[:message] || ''
   messages = {'' => '', 'added' => 'Thanks, for joining our mailing list.', 'exists' => 'You have already joined our mailing list'}
-	erb :index, :locals => {:message => messages[message]}
+    erb :index, :locals => {:message => messages[message]}
 end
-=======
->>>>>>> 41e9efccb83498003dabe645cc14e72056646a9f
+
 
 #checks to see
 #def db()
@@ -41,7 +40,7 @@ end
 #
 #message = params[:message] || ''
 #  messages = {'' => '', 'added' => 'Thanks, for joining our mailing list.', 'exists' => 'You have already joined our mailing list'}
-#	erb :index, :locals => {:message => messages[message]}
+#    erb :index, :locals => {:message => messages[message]}
 #end
 #
 get '/contact'do
@@ -53,7 +52,7 @@ get '/contact'do
   deliver = params[:deliver] || ''
   messages = {'' => '', 'success' => "Thank you for your message. We'll get back to you shortly.", 'error' => 'Sorry, there was a problem delivering your message.'}
   message = messages[deliver]
- 
+
     erb :contact, :locals => {thanks: thanks, num1: num1, num2: num2, sum: sum, message: message }
 end
 
@@ -67,24 +66,24 @@ reason = params[:reason]
 sum = params[:sum]
 
  robot = params[:robot]
-  
+
   if robot == sum
     Pony.mail(
         :to => "#{email}",
-        :cc => 'info@coalitionforabrightergreene.org', 
+        :cc => 'info@coalitionforabrightergreene.org',
         :from => 'joseph@minedminds.org',
-        :subject => "CBG", 
-        :content_type => 'text/html', 
+        :subject => "CBG",
+        :content_type => 'text/html',
         :body => erb(:email2,:layout=>false),
-        :via => :smtp, 
+        :via => :smtp,
         :via_options => {
           :address              => 'smtp.gmail.com',
           :port                 => '587',
           :enable_starttls_auto => true,
            :user_name           => ENV['email'],
            :password            => ENV['email_pass'],
-           :authentication       => :plain, 
-           :domain               => 'localhost:4567' 
+           :authentication       => :plain,
+           :domain               => 'localhost:4567'
         }
 
       )
@@ -117,7 +116,7 @@ end
 #
 
 get '/' do
- 
+
  erb :commingsoon
 
 end
@@ -131,7 +130,7 @@ post '/subscribe' do
   email = params[:email]
 
  check_email = db.exec("SELECT * FROM mailing_list WHERE email = '#{email}'")
-     
+
   if check_email.num_tuples.zero? == false
     db.close
     redirect '/?message=exists'
@@ -144,33 +143,33 @@ end
 #
 #get '/manifesto'do
 #  signed = db.exec("SELECT * FROM manifesto")
-#   
+#
 # erb :manifesto, :locals => {signed: signed}
-# 
+#
 #end
 #
 #post '/manifesto' do
-# 
+#
 #name = params[:name]
 #email_address = params[:email_address]
 # time = Time.new
 # date = time.strftime("%Y-%m-%d")
-# 
+#
 # db.exec("INSERT INTO manifesto (name,email_address,date) values ('#{name}','#{email_address}','#{date}')")
-# 
+#
 # redirect ('/manifesto')
 #end
 #
 #get '/current_happenings' do
-# 
+#
 # erb :current_happenings
-# 
+#
 #end
 #
 #get '/404' do
-# 
+#
 # erb :ohno
-# 
+#
 #end
 #
 #not_found do
@@ -178,12 +177,12 @@ end
 #end
 #
 #get '/new' do
-# 
+#
 #erb :new
 #end
 #
 #get '/volunteer'do
-# 
+#
 # thanks = params[:thanks] || ''
 #  num1 = rand(9)
 #  num2 = rand(9)
@@ -191,52 +190,52 @@ end
 #  deliver = params[:deliver] || ''
 #  messages = {'' => '', 'success' => "Thank you for your message. We'll get back to you shortly.", 'error' => 'Sorry, there was a problem delivering your message.'}
 #  message = messages[deliver]
-# 
+#
 #    erb :volunteer, :locals => {thanks: thanks, num1: num1, num2: num2, sum: sum, message: message }
 #end
 #
 #
 #
 #post '/volunteer' do
-# 
+#
 #  num1 = rand(9)
 #  num2 = rand(9)
 #  sum = num1 + num2
 #
 # erb :volunteer, :locals => {thanks: thanks, num1: num1, num2: num2, sum: sum, message: message }
-# 
+#
 #end
 
-<<<<<<< HEAD
+
 get '/manifesto'do
   signed = db.exec("SELECT * FROM manifesto")
-   
+
  erb :manifesto, :locals => {signed: signed}
- 
+
 end
 
 post '/manifesto' do
- 
+
 name = params[:name]
 email_address = params[:email_address]
  time = Time.new
  date = time.strftime("%Y-%m-%d")
- 
+
  db.exec("INSERT INTO manifesto (name,email_address,date) values ('#{name}','#{email_address}','#{date}')")
- 
+
  redirect ('/manifesto')
 end
 
 get '/current_happenings' do
- 
+
  erb :current_happenings
- 
+
 end
 
 get '/404' do
- 
+
  erb :ohno
- 
+
 end
 
 not_found do
@@ -244,12 +243,12 @@ not_found do
 end
 
 get '/new' do
- 
+
 erb :new
 end
 
 get '/volunteer'do
- 
+
  thanks = params[:thanks] || ''
   num1 = rand(9)
   num2 = rand(9)
@@ -257,7 +256,7 @@ get '/volunteer'do
   deliver = params[:deliver] || ''
   messages = {'' => '', 'success' => "Thank you for your message. We'll get back to you shortly.", 'error' => 'Sorry, there was a problem delivering your message.'}
   message = messages[deliver]
- 
+
     erb :volunteer, :locals => {thanks: thanks, num1: num1, num2: num2, sum: sum, message: message }
 end
 
@@ -270,7 +269,6 @@ post '/volunteer' do
   sum = num1 + num2
 
  erb :volunteer, :locals => {thanks: thanks, num1: num1, num2: num2, sum: sum, message: message }
- 
+
 end
-=======
->>>>>>> 41e9efccb83498003dabe645cc14e72056646a9f
+
