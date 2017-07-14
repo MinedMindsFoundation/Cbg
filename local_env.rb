6fc648ce765ba9d76b1627adbdb691d0caa7a46d
Heather Shockney <heather.shockney@minedminds.org>
2017-07-14 10:53:40 -0400

    Delete local_env.rb

--- local_env.rb
+++
@@ -1,8 +0,0 @@
-ENV['host'] = 'greenecoalition.ccar1za80ci4.us-west-2.rds.amazonaws.com'
-ENV['port'] = '5432'
-ENV['dbname'] = "greenecoalition"
-ENV['user'] = 'MinedMinds'
-ENV['password'] = 'Min3dMinds'
-
-ENV['email']= 'heather.shockney@minedminds.org'
-ENV['email_pass'] ='Sjs@3042764079'
